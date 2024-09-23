37c93421c53c078b9f1db50fcdf1997f8a04311b
Jon-Mikel <garrisonhgrave@yahoo.com>
2024-09-23 10:37:38 -0500

    typescript demo

---
+++ TypeScript/TypeScriptDemo1/src/index.ts
@@ -0,0 +1,15 @@
+// here is an array of strings
+let dogArray: string[] = ["Atlas", "Ranger", "Tony"];
+// array of numbers
+let numArray: number[] = [14, 23, 35];
+let movieArr: object[] = [];
+
+// function
+// data types for params and args,
+// data types for returned values
+const getAge = (num: number, fname: string = "JM", isMarried: boolean = false): number => {
+  return num * 4;
+};
+let result = getAge(34);
+console.log(result);
+
