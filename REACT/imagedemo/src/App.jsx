c501406d33e73c3fbcc38fe192fe538f26c89324
Jon-Mikel <garrisonhgrave@yahoo.com>
2024-09-30 11:11:32 -0500

    initial ush

---
+++ REACT/imagedemo/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import doggy from "./assets/ireland.jpeg"
+
+function App() {
+
+
+  return (
+    <>
+      <h1>My Image Demo</h1>
+      {/* <img src={doggy} alt="irish countryside" />
+      <img src="./public/images/blue_frenchie.webp" alt="" /> */}
+      
+    </>
+  )
+}
+
+export default App
