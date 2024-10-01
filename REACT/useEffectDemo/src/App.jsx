6108fd85b7b015410db8ef120f61a8b8adc1f020
Jon-Mikel <garrisonhgrave@yahoo.com>
2024-10-01 09:25:55 -0500

    initial push

---
+++ REACT/useEffectDemo/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  // setCount(count + 1)
+
+  useEffect(()=>{
+    if (count < 5){
+      setCount(count + 1)
+    }
+  }, [count, doggy, cat])
+
+  return (
+    <>
+      <h1>My Counter Demo with Use Effect</h1>
+      COUNT: {count}
+    </>
+  )
+}
+
+export default App
