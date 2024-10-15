6427c741f2c5c26390bb80499f26270b374d3a4b
Jon-Mikel <garrisonhgrave@yahoo.com>
2024-10-15 08:55:59 -0500

    updated

--- REACT/MUIForms/src/components/Form.jsx
+++ REACT/MUIForms/src/components/Form.jsx
@@ -6,16 +6,43 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { object, string, number } from "yup";
 
 export default function Form() {
-  const person = {};
-  const { register, handleSubmit, setValue, watch } = useForm();
+  const personSchema = object({
+    first_name: string().required("Must have a first name").min(2),
+    last_name: string().max(3, "Last name must be less than 3 characters."),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(personSchema),
+  });
 
   const handleChange = (e) => {
     setValue(e.target.name, e.target.value);
+    // use watch to see "data"
     console.log(watch("first_name"));
+    // without arguments, we see everything
+    console.log(watch());
   };
 
   const onSubmit = (data) => {
-    console.log(data);
+    // console.log(data);
+    let JSONData = JSON.stringify(data)
+    // eventually 
+    // let options = {
+    //   method: "POST",
+    //   body: JSONData
+    // }
+    // axios.request(options)
+    // .then()
+    // .catch()
+    console.log(JSONData);
+    reset();
   };
 
   return (
@@ -32,12 +59,25 @@ export default function Form() {
         variant="outlined"
         {...register("first_name")}
         onChange={handleChange}
+        helperText={
+          errors.first_name && <span>{errors.first_name.message}</span>
+        }
+        error={errors.first_name !== undefined}
+      />
+      <TextField
+        id="outlined-basic"
+        label="Last Name"
+        variant="outlined"
+        {...register("last_name")}
+        onChange={handleChange}
+        helperText={errors.last_name && <span>{errors.last_name.message}</span>}
+        error={errors.last_name !== undefined}
       />
       <Stack direction="row" spacing={2}>
         <Button type="submit" variant="contained" color="success">
           Submit
         </Button>
-        <Button variant="contained" color="error">
+        <Button onClick={()=> reset()} variant="contained" color="error">
           Reset
         </Button>
       </Stack>
